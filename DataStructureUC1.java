a20d49b5d3690c108fd2547569b0a1c1a994ac10
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-25 22:51:08 +0530

    simple linkedlist

---
+++ DataStructureUC1.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        LinkedList<String> i = new LinkedList<String>();
+        i.add("30");
+        i.add("56");
+        i.add("70");
+        System.out.println(i);
+    }
+}
